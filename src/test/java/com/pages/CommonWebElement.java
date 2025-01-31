8eb5842fc5aa12cc938806ea502328018d5e31b4
Kala-Krishnan <kala.krishnan1107@gmail.com>
2025-01-31 17:06:56 -0500

    Common Web Element New class File

---
+++ src/test/java/com/pages/CommonWebElement.java
@@ -0,0 +1,40 @@
+package com.pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
+public class CommonWebElement extends BaseClassAppium{
+
+	@AndroidFindBy(xpath = "(//android.widget.ImageView[@resource-id=\"free.rm.skytube.extra:id/thumbnail_image_view\"])[1]")
+	private WebElement firstVideo;
+	
+	@AndroidFindBy(id="free.rm.skytube.extra:id/skipButton")
+	private WebElement skipButton;
+	
+	WebDriverWait wait;
+	
+	public CommonWebElement() {
+		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+	}
+	public void clickFirstVideo()
+	{
+		wait.until(ExpectedConditions.elementToBeClickable(firstVideo));
+		firstVideo.click();
+	}
+	public void skipButton()
+	{
+		wait.until(ExpectedConditions.elementToBeClickable(skipButton));
+		skipButton.click();
+	}
+	
+	
+}
